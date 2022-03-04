6533156a5ef1310a5e7660509fb699d6c8d409ae
leevesper <leevesper2013@gmail.com>
2022-03-03 17:01:55 -0700

    Added working back arrow to "LoginActivity".

--- app/src/main/java/com/example/mainapplicationproject/LoginActivity.java
+++ app/src/main/java/com/example/mainapplicationproject/LoginActivity.java
@@ -10,5 +10,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
 }
