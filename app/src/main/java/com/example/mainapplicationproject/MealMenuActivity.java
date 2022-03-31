4a9d8e68525278b1ac3b6fcff53fce8f3bc57cc6
leevesper <leevesper2013@gmail.com>
2022-03-31 16:33:54 -0600

    Updated.

--- app/src/main/java/com/example/mainapplicationproject/MealMenuActivity.java
+++ app/src/main/java/com/example/mainapplicationproject/MealMenuActivity.java
@@ -3,9 +3,17 @@ package com.example.mainapplicationproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+// This activity is designed to help filter which recipes will be shown to the user from the main
+// recipe database. When the user clicked one of the four buttons, a variable is assigned based on
+// which button is clicked and the variable will be passed to the next activity and used to filter
+// which recipes will be shown based on the category selected.
+
+// Main database and functionality to pass variable to the next activity has not been created.
 
 public class MealMenuActivity extends AppCompatActivity {
 
@@ -13,6 +21,7 @@ public class MealMenuActivity extends AppCompatActivity {
     private Button lunchButton;
     private Button dinnerButton;
     private Button desertButton;
+    //Variable to hold the selection variable to pass to the next activity.
     public String selection;
 
     @Override
@@ -20,8 +29,9 @@ public class MealMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meal_menu);
 
-        ////////
+        // Creates action bar and sets action bar color to black.
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.black)));
 
         breakfastButton = (Button) findViewById(R.id.buttonFruits);
         breakfastButton.setOnClickListener(new View.OnClickListener() {
